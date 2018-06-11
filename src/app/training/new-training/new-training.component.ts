39150e014de862c7fe0d78758dbab9c63a1ea200
Alejandro Vazquez <alejandro@MacBook-Pro-de-Alejandro.local>
2018-06-11 11:42:58 +0200

    Current training component updated with a mat progress spinner and mat dialog

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-new-training',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
+  @Output() newTraining = new EventEmitter<any>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onNewTraining() {
+    this.newTraining.emit();
   }
 
 }
