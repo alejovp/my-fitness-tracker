2efdbb95365faf3af21519ec55720d49976580b8
Alejandro Vazquez <alejandro@MacBook-Pro-de-Alejandro.local>
2018-07-15 21:53:06 +0200

    using snapshot instead of valueChanges in order to get the id from the db

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
-import { firestore } from 'firebase';
 
 @Component({
   selector: 'app-new-training',
@@ -12,7 +13,7 @@ import { firestore } from 'firebase';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
-  exerciseList: Exercise[] = [];
+  exerciseList: Observable<Exercise[]>;
 
   constructor(
     private trainingService: TrainingService,
@@ -20,12 +21,19 @@ export class NewTrainingComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.db
+    this.exerciseList = this.db
       .collection('availableExercises')
-      .valueChanges()
-      .subscribe(result => {
-        console.log(result);
-      });
+      .snapshotChanges()
+      // mapping the snapshot response to the Exercice model (we need the db document id)
+      .pipe(map(docsArray => {
+          return docsArray.map(document => {
+            return { id: document.payload.doc.id, ...document.payload.doc.data() } as Exercise;
+          });
+        })
+      );
+      // .subscribe(result => {
+      //   console.log(result);
+      // });
   }
 
   onNewTraining(form: NgForm) {
